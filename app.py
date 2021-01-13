299b7a4b7eccbe7b9c22997d8ee9207d8834ad28
Stuart Saint <stuartsaint242@gmail.com>
2021-01-13 18:40:28 +0000

    Modify: more CSS styling

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/pubs")
 def pubs():
     pubs = mongo.db.pubs.find()
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,16 @@
 /* jQuery for interactive design */
 
+/* jshint esversion: 6 */
+/* globals $:false */
+/* Comments above tells JSHint what version of JS is being used, and secondly overrides JSHint's assumption that the 
+$ symbol is an undefined variable */
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
+});
+
+/* add hover here to convert menu options to font awesome icons on hover */
+
+$("#pintButton").mouseeenter(function(){
+  $(this).html("Hello <b>world</b>!");
 });
