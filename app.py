7f6fa9947e6bab3bb31aff18cfd9ecb4e0328774
Stuart Saint <stuartsaint242@gmail.com>
2021-01-27 20:58:35 +0000

    Testing of edit profile method failed

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import (
+    generate_password_hash, check_password_hash)
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
